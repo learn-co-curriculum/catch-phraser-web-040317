01fcac485255c69f5be30d4b6df4c2e41cf9b900
kavunshiva <kavunshiva@gmail.com>
2017-05-23 11:59:04 -0400

    make it pass tests (should i have used #show, though?)

--- app/controllers/api/v1/catch_phrases_controller.rb
+++ app/controllers/api/v1/catch_phrases_controller.rb
@@ -7,5 +7,26 @@ class Api::V1::CatchPhrasesController < ApplicationController
 
   def create
     # this should create a new catch phrase
+    @catch_phrase = CatchPhrase.new(
+      catch_phrase_params(
+        :suitable_for_work,
+        :mood,
+        :content,
+        :student_id
+      )
+    )
+    if @catch_phrase.save()
+      render json: @catch_phrase
+    end
+  end
+
+  # def show
+  #   byebug
+  #   catch_phrase = CatchPhrase.find_by(id: params[:id])
+  #   render json: catch_phrase
+  # end
+
+  def catch_phrase_params(*args)
+    params.require(:catch_phrase).permit(*args)
   end
 end
